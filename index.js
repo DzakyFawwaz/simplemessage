97908cfa154eb01ef0505f62b82ed0980360ce50
Dzaky <dzkyfwzaryd@gmail.com>
2022-04-28 15:39:26 +0700

    fixing

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ function simplemessage(
   message = "I Love You"
 ) {
   const compiledMessage = `Hi ${to}, this is a message from ${from}.\n${message}`;
-  console.log(compiledMessage);
+//   console.log(compiledMessage);
   return compiledMessage;
 }
 
-// simplemessage("Dzaky", "My Mom", "I Need Money");
+module.export = simplemessage();
 
-module.export = simplemessage;
+// const simplemessage = require("ky-simplemessage");
+let msg = simplemessage()
+console.log(msg)
+
+// simplemessage()
