ad80c9c548a9fb3f94957ed8251aba7f8b65d05c
Dzaky <dzkyfwzaryd@gmail.com>
2022-04-28 15:19:06 +0700

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+function simplemessage(
+  from = "Anonymous",
+  to = "My Beloved Ones",
+  message = "I Love You"
+) {
+  const compiledMessage = `Hi ${to}, this is a message from ${from}.\n${message}`;
+  console.log(compiledMessage);
+  return compiledMessage;
+}
+
+// simplemessage("Dzaky", "My Mom", "I Need Money");
+
+module.export = simplemessage;
